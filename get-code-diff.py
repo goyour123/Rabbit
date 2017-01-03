615ded20414e2d6cab74fa5fd3955723e03bf81c
ren.yang <goyour123@gmail.com>
2017-01-04 00:12:29 +0800

    git-code-diff consumes ini file

--- get-code-diff.py
+++ get-code-diff.py
@@ -1,13 +1,14 @@
 import git
+import configparser
 
-repo = git.Repo('D:\Python\Python3.5\Traffly')
+config = configparser.ConfigParser()
+config.read('setting.ini')
+
+repo = git.Repo(config['git']['repo'])
 
 commits_list = list(repo.iter_commits())
 
-commit = repo.commit('SHA-1')
+commit = repo.commit(config['commit']['sha'])
 pre_commit = commit.parents[0]
 
 diffs = commit.diff(pre_commit)
-
-for file in diffs:
-    print(file)
