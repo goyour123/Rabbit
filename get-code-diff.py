1af89f5d7c14a6ffc93c1047a7c5371824ed10a0
ren.yang <goyour123@gmail.com>
2017-01-15 18:32:48 +0800

    Reduce algorithm of code-change path

--- get-code-diff.py
+++ get-code-diff.py
@@ -21,14 +21,16 @@ config = configparser.ConfigParser()
 config.read('setting.ini')
 
 dst_path = config['os']['target']
+dst_mod_path = dst_path + '/Modified'
+dst_org_path = dst_path + '/Original'
 
-if os.path.isdir(dst_path + '/Modified'):
-    shutil.rmtree(dst_path + '/Modified')
-os.mkdir(dst_path + '/Modified')
+if os.path.isdir(dst_mod_path):
+    shutil.rmtree(dst_mod_path)
+os.mkdir(dst_mod_path)
 
-if os.path.isdir(dst_path + '/Original'):
-    shutil.rmtree(dst_path + '/Original')
-os.mkdir(dst_path + '/Original')
+if os.path.isdir(dst_org_path):
+    shutil.rmtree(dst_org_path)
+os.mkdir(dst_org_path)
 
 repo_path = config['git']['repo']
 repo = git.Repo(config['git']['repo'])
@@ -39,15 +41,12 @@ pre_commit = commit.parents[0]
 
 diffs = commit.diff(pre_commit)
 
-dst_path_mod = dst_path + '/Modified'
-dst_path_org = dst_path + '/Original'
-
 tree_files = []
 
 repo_git.checkout(commit)
 
 for f in diffs:
-    dst_path_target = dir_tree_creator(f.a_blob.path, dst_path_mod)
+    dst_path_target = dir_tree_creator(f.a_blob.path, dst_mod_path)
     tree_files.append(f.a_blob.path)
     src_path = repo_path + '/' + f.a_blob.path
     if os.path.isfile(src_path):
@@ -56,9 +55,9 @@ for f in diffs:
 repo_git.checkout(pre_commit)
 
 for f in tree_files:
-    dst_path_target = dir_tree_creator(f, dst_path_mod)
+    dst_path_target = dir_tree_creator(f, dst_mod_path)
     src_path = repo_path + '/' + f
     if os.path.isfile(src_path):
-        shutil.copy(src_path, dst_path_org)
+        shutil.copy(src_path, dst_org_path)
 
 repo_git.checkout('master')
