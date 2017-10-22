a0c549c778495d1fd3428e8327000ad96f9f51da
Ren.Yang <goyour123@gmail.com>
2017-10-23 00:50:20 +0800

    Add comment in Rabbit.sh for next thing to do.

--- Rabbit.sh
+++ Rabbit.sh
@@ -2,7 +2,11 @@
 repo='D:\Python\Python2.7\Pratice'
 output='D:\Python\Python3.5\Rabbit\Output'
 
+# Remove output directory to ensure that files in it are correct.
 #rm -r $output
+
+# Create output directory for checkout solution.
+
 
 cd $repo
 diff_file=$(git diff --name-only)
