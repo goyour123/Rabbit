393e1dc9619610510686b89c302561d5948b48c6
Ren.Yang <goyour123@gmail.com>
2017-11-10 00:05:44 +0800

    Copy files and create directory tree from repo to the output folder

--- Rabbit.sh
+++ Rabbit.sh
@@ -1,5 +1,5 @@
 
-repo='D:\Python\Python2.7\Pratice'
+repo='D:\Python\Python2.7\bios-toolbox'
 output='D:\Python\Python3.5\Rabbit\Output'
 
 # Remove output directory to ensure that files in it are correct.
@@ -13,13 +13,15 @@ diff_file=$(git diff --name-only)
 
 for file_path in $diff_file
   do
-    ifs_org=$IFS
-    IFS='/'
-    for folder in $file_path
-      do
-        echo $folder
-      done
-    IFS=$ifs_org
-
-    #cp -r $file_path $output\\$file_path
+    file_name=${file_path##*/}
+    if [ ${file_path%/*} == $file_name ]; then
+      file_dir=""
+    else
+      file_dir=${file_path%/*}
+    fi
+    mkdir -p $output/$file_dir
+    cp -R $file_path $output/$file_path
   done
+
+# Pause for the result
+#read -rsn 1 key
